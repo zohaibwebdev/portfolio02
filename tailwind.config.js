6f95a2585728783e35375c1181d41f6682dfd219
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-08-31 11:46:59 +0500

    new profile website project started🎉

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens:{
+        sm:"480px",
+        md:"768px",
+        lg:"1024px",
+        xl:"1280px",
+      },
+      spacing:{
+        'big':"48rem"
+      },
+      colors:{
+        "primary":"#FFEB3B"
+      }
+    },
+    fontFamily:{
+      nunito:['Nunito',' sans-serif']
+    }
+  },
+  plugins: [],
+}
